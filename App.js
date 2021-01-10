89dc497a35f652097f6643cab39c56c8b7e4ef6d
cristianiniguez <felizpc1@gmail.com>
2021-01-10 16:25:44 -0400

    avanzado: agregando tabs de navegacion

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import {Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
 import CoinsStack from './src/components/Coins/CoinsStack';
+
+import colors from 'cryptoTracker/src/res/colors';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor: '#fefefe',
+          // activeTintColor: ''
+          style: {
+            backgroundColor: colors.blackPearl,
+          },
+        }}>
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                source={require('cryptoTracker/src/assets/bank.png')}
+                style={{tintColor: color, width: size, height: size}}
+              />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
